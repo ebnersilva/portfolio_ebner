f7564a0b1dd206239be853a7b3857d91cf6c1eef
Ebner Silva <ebner.suporte@hotmail.com>
2020-08-03 09:59:03 -0300

    feat: Initial Commit

--- src/pages/index.js
+++ src/pages/index.js
@@ -2,6 +2,6 @@ import React from 'react';
 
 export default function Home() {
   return (
-    <h1>Site do Ebner - Feito em NextJS</h1>
+    <h1>Site do Ebner - Feito em NextJS - v1.00-001-001</h1>
   )
 }
